ed1c36899d2a3d74ded9ccbef778b8c120a9f381
Asem130 <asemelgendi13@gmail.com>
2024-11-20 23:40:43 +0200

    adding login states

--- lib/features/auth/logic/login_cubit/login_cubit.dart
+++ lib/features/auth/logic/login_cubit/login_cubit.dart
@@ -1,11 +1,9 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:taqwa/features/auth/data/models/login_request_body.dart';
 import 'package:taqwa/features/auth/data/repos/login_repo.dart';
 import 'package:taqwa/features/auth/logic/login_cubit/login_state.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-@freezed
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
   LoginCubit(this._loginRepo) : super(const LoginState.initial());
